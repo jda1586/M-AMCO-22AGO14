58516272307825388c1048dc4e34bab3e4f33322
Pedro Luna <pedro@maniak.com.mx>
2014-08-25 18:34:16 -0500

    AMCO BASE

---
+++ app/views/hello.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>AMCO</title>
+    <link href="/css/style.css" rel="stylesheet">
+</head>
+<body>
+    <div id="content">
+        <div class="content-header">
+            <div class="text-name">
+                Colegio Albert Camus
+            </div>
+            <div class="top-text">
+                <div>
+                    Primaria, secundaria y preparatoria
+                </div>
+                <div>
+                    Ley #3080, Colonia Residencial Juan Manuel C.P. 44670
+                </div>
+            </div>
+        </div>
+        <div class="content-main">
+            <div class="one-column">A</div>
+            <div class="two-column">B</div>
+            <div class="three-column">C</div>
+        </div>
+        <div class="content-footer">
+
+        </div>
+    </div>
+</body>
+</html>
